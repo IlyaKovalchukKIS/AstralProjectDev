1f5e14f8fe933d09d3af9a2890b5c03bf0dcc907
ilakovalcuk <mrlivedance@vk.com>
2026-02-26 00:44:38 +0300

    init project

---
+++ tarot_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TarotAppConfig(AppConfig):
+    name = 'tarot_app'
---
+++ tarot_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'tarot_app'
+
+urlpatterns = [
+    # path('tarot/', views.tarot_view, name='tarot'),
+]
